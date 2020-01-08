74998331d088c974727559415b6f0d4277d9ab7e
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-01-08 15:53:26 -0600

    Refactor markup into functional components

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+export default () => `
+<footer>
+  &copy;2020 Oscar Gordillo
+</footer>
+`;
---
+++ components/Nav.js
@@ -0,0 +1,14 @@
+function createListHTML(links) {
+  //Use join to change the array and make it a string separated by spaces
+  return links
+    .map(link => `<li class ="btn"><a href="#">${link}</a></li>`)
+    .join(" ");
+}
+
+export default st => `
+<nav>
+<ul class="flex justify-space-around">
+  ${createListHTML(st)}
+</ul>
+</nav>
+`;
