54930975cee059db0e751a3becbbe13c743bd718
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-01-18 10:39:42 -0600

    Add deck API using axios

--- scripts.js
+++ scripts.js
@@ -1,13 +1,37 @@
+import axios from "axios";
+
 // let deckID = null;
 
-// axios
-//   .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-//   .then(response => {
-//     deckID = response.data.deck_id;
-//   });
-
-// function drawCards(numOfCards = 2) {
+// function drawCards() {
+//   axios
+//     .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+//     .then(response => {
+//       deckID = response.data.deck_id;
+//       console.log(deckID);
+//     });
+// }
+// console.log(deckID);
 //   axios
 //     .get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
-//     .then(response => (state.currentCards = response.data.cards));
-// }
+//     .then(response => console.log(response));
+// //}
+
+// console.log(drawCards());
+
+const createDeck = () => {
+  return axios
+    .get("https://deckofcardsapi.com/api/deck/new/")
+    .then(response => {
+      return response.data;
+    });
+};
+createDeck()
+  .then(data => {
+    console.log(data.deck_id);
+    return data.deck_id;
+  })
+  .then(deckId =>
+    axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=2
+`)
+  )
+  .then(data => console.log(data.data));
