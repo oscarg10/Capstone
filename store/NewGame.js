145a9996d7c1a0b688b66bd08c0df01cf0df41e5
Manav Misra <manavm@visionwebsoft.com>
2020-01-21 13:10:54 -0600

    Add state for cards.
    This should have been 'commit'ted earlier!

--- store/NewGame.js
+++ store/NewGame.js
@@ -1,4 +1,14 @@
 export default {
   heading: "New",
-  mainContent: "New"
+  mainContent: "New",
+  deckID: null,
+
+  // TODO{oscar}: Consider an ARRAY of players for multi-player games where each player has an 'isCurrent' attribute that gets updated if it's their turn.
+
+  currentPlayer: {
+    // Updated if/when user logs in (https://firebase.google.com/docs/auth/admin/manage-users)
+    id: null,
+    cards: []
+  },
+  dealerCards: []
 };
